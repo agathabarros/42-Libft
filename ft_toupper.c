bc28656059b489a896e877a68f7ea69d5e5a3afc
Agatha Pereira De Barros Da Costa <agpereir@c1r4s2.42porto.com>
2023-05-04 15:53:00 +0100

    without evaluation

--- ft_toupper.c
+++ ft_toupper.c
@@ -1,12 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   index.c                                            :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agpereir <agpereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/03 15:52:02 by agpereir          #+#    #+#             */
-/*   Updated: 2023/05/03 15:52:03 by agpereir         ###   ########.fr       */
+/*   Created: 2023/04/14 17:36:10 by agpereir          #+#    #+#             */
+/*   Updated: 2023/05/04 15:22:45 by agpereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Convert lowercase letters to uppercase.*/
+#include "libft.h"
+
+int	ft_toupper(int arg)
+{
+	if ((arg >= 'a' ) && (arg <= 'z'))
+		return (arg -= 32);
+	else
+		return (arg);
+}
